8fdd1bc3b975eeb6f1de8a499c5fbdb3107407a9
Christian <Futte8D@hotmail.com>
2021-08-18 23:44:52 +0200

    Updated routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 const routes: Routes = [
-  { path: '/', component: HomeComponent },
+  { path: '', component: HomeComponent },
 ]
 @NgModule({
   declarations: [
